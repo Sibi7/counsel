2626aa6792af6792118fec5a00dacaeedef29901
Sibi7 <Sibi_7@mail.ru>
2017-08-28 14:15:37 +0300

    add animation links

--- app/js/script.js
+++ app/js/script.js
@@ -20,26 +20,23 @@ $(document).ready(function () {
             }
         }
     });
-    // $('.car').owlCarousel({
-    //     loop:true,
-    //     margin:0,
-    //     nav: false,
-    //     responsiveClass:true,
-    //     // navText: ["<i class='fa fa-2x fa-angle-left'></i>","<i class='fa fa-2x fa-angle-right'></i>"],
-    //     responsive:{
-    //         0:{
-    //             items:1,
-    //             nav:true
-    //         },
-    //         600:{
-    //             items:1,
-    //             nav:false
-    //         },
-    //         1000:{
-    //             items:1,
-    //             nav:false,
-    //             loop:true
-    //         }
-    //     }
-    // })
+    /*go to top scroll*/
+    $.fn.scrollToTop = function () {
+        $(this).hide().removeAttr("href");
+        if ($(window).scrollTop() >= "250") $(this).fadeIn("slow");
+        var scrollDiv = $(this);
+        $(window).scroll(function () {
+            if ($(window).scrollTop() <= "250") {
+                $(scrollDiv).fadeOut("slow")
+            }
+            else {
+                $(scrollDiv).fadeIn("slow")
+            }
+        });
+        $(this).click(function () {
+            $("html, body").animate({scrollTop: 0}, "slow")
+        })
+    };
+    $('#Go_Top').scrollToTop();
+    /*close go to top scroll*/
 });
