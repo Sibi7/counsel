98db73ee93247e3278bc8a1e0e70264c8f9e256c
AKD47 <van83horn@gmail.com>
2017-09-05 14:46:44 +0300

    contacts page

--- app/js/script.js
+++ app/js/script.js
@@ -1,29 +1,48 @@
-$(document).ready(function () {
-    
+if ($('#map').length > 0) {
+    var map = new Map();
+    map.init({
+        selector: '#map',
+        center: $('.concreate-adress').html(),
+        zoom: 17,
+        placemarks: [
+            {
+                address: $(".concreate-adress").html(),
+                options: [
+                    {key: 'draggable', value: false},
+                    {key: 'iconLayout', value: 'default#image'},
+                    {key: 'iconImageHref', value: '/img/icons/map-marker-icon.png'}
+                ]
+            }
+        ]
+    });
+}
+
+$(document).ready(function() {
+
     /*header slider*/
     $('.top-slider').owlCarousel({
-        loop:true,
-        margin:0,
-        responsiveClass:true,
-        navText: ["<i class='fa fa-2x fa-angle-left'></i>","<i class='fa fa-2x fa-angle-right'></i>"],
-        responsive:{
-            0:{
-                items:1,
-                nav:true
+        loop: true,
+        margin: 0,
+        responsiveClass: true,
+        navText: ["<i class='fa fa-2x fa-angle-left'></i>", "<i class='fa fa-2x fa-angle-right'></i>"],
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
             },
-            600:{
-                items:1,
-                nav:true
+            600: {
+                items: 1,
+                nav: true
             },
-            1000:{
-                items:1,
-                nav:true,
-                loop:true
+            1000: {
+                items: 1,
+                nav: true,
+                loop: true
             }
         }
     });
     /*close*/
-    
+
     /*go to top scroll*/
     $.fn.scrollToTop = function () {
         $(this).hide().removeAttr("href");
@@ -45,61 +64,61 @@ $(document).ready(function () {
     /*close go to top scroll*/
 
 
-    $( ".header__menu__list" ).click(function() {
-        $( "#dropdown" ).slideToggle( "slow", function() {
+    $(".header__menu__list").click(function () {
+        $("#dropdown").slideToggle("slow", function () {
             // Animation complete.
         });
     });
     //js
     $(document).ready(function () {
-        var link =$('.menu-link');
+        var link = $('.menu-link');
         var link_active = $('.menu-link_active');
 
         link.click(function () {
             link.toggleClass('menu-link_active');
         })
     });
-    $( ".menu-link" ).click(function() {
-        $( ".header__menu" ).slideToggle( "slow", function() {
+    $(".menu-link").click(function () {
+        $(".header__menu").slideToggle("slow", function () {
         });
     });
-    
+
     /*services dropdown*/
     $(document).on('click', '.promotion__right-menu > li > a', function () {
-       event.preventDefault();
-       dropdownList = $(this).next('.dropdown');       
-       if($(this).hasClass('active')){
-           $(this).removeClass('active');
-           dropdownList.slideUp(500);
-       } else{
-           $('.promotion__right-menu > li > a').removeClass('active');
-           $('.promotion__right-menu > li > .dropdown').slideUp(500);
-           $(this).addClass('active');
-           dropdownList.slideDown(500);
-       }
+        event.preventDefault();
+        dropdownList = $(this).next('.dropdown');
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            dropdownList.slideUp(500);
+        } else {
+            $('.promotion__right-menu > li > a').removeClass('active');
+            $('.promotion__right-menu > li > .dropdown').slideUp(500);
+            $(this).addClass('active');
+            dropdownList.slideDown(500);
+        }
     });
-    /*close*/    
-    
+    /*close*/
+
     /*rev slider*/
     $('.about__rev-slider').owlCarousel({
         loop: false,
-        autoplay:true,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        margin:0,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        margin: 0,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
             },
-            600:{
-                items:1,
-                nav:true
+            600: {
+                items: 1,
+                nav: true
             },
-            1000:{
-                items:1,
-                nav:true
+            1000: {
+                items: 1,
+                nav: true
             }
         }
     });
